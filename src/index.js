1d16b5d6e0feca5f654844d0ee44b3e7cf951d3d
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-07-12 16:41:29 -0500

    Updating files
    Updating HTML, index.js, and style.css for milestone

--- src/index.js
+++ src/index.js
@@ -8,38 +8,18 @@ const reloadIcon = document.getElementById('reload-img');
 const enterIcon = document.getElementById('enter-icon');
 enterIcon.src = enter;
 reloadIcon.src = reload;
-const todoList = [
-  {
-    id: 1,
-    description: 'Starting project structure',
-    completed: false,
-  },
-  {
-    id: 2,
-    description: 'Initiate writing JS code',
-    completed: false,
-  },
-  {
-    id: 3,
-    description: 'Wash my Dog',
-    completed: true,
-  },
-  {
-    id: 4,
-    description: 'Pray ',
-    completed: false,
-  },
-];
+
+let todoList = [];
 
 const renderList = (list) => {
   let innerList = '';
   if (list.length === 0) {
-    innerList = '<h3 class="list-placeholder">Add your first task!</h3>';
+    innerList = '<h3 class="list-placeholder">Please, add your first task!</h3>';
   } else {
-    const sortedList = list.sort((a, b) => a.id - b.id);
+    const sortedList = list.sort((a, b) => a.index - b.index);
     sortedList.forEach((task) => {
       innerList += `
-          <li class="to-do-tasks">
+          <li class="to-do-tasks" data-task-id="${task.id}">
             <div class="check-box ${task.completed ? 'completed' : ''}">${task.completed ? '✓' : ''}</div>
             <p class="task-description ${task.completed ? 'line-through' : ''}">${task.description}</p>
             <img class="more-logo" src="${moreIcon}"/>
@@ -50,4 +30,95 @@ const renderList = (list) => {
   ToDoListContainer.innerHTML = innerList;
 };
 
-window.onload = renderList(todoList);
+const saveToLocalStorage = (list) => {
+  localStorage.setItem('todoList', JSON.stringify(list));
+};
+
+const addTask = (description) => {
+  const newTask = {
+    id: Date.now(),
+    description,
+    completed: false,
+    index: todoList.length + 1,
+  };
+  todoList.push(newTask);
+  saveToLocalStorage(todoList);
+  renderList(todoList);
+};
+
+const editTaskDescription = (taskId, newDescription) => {
+  todoList.forEach((task) => {
+    if (task.id === taskId) {
+      task.description = newDescription;
+    }
+  });
+  saveToLocalStorage(todoList);
+  renderList(todoList);
+};
+
+const clearCompletedTasks = () => {
+  todoList = todoList.filter((task) => !task.completed);
+  todoList.forEach((task, index) => {
+    task.index = index + 1;
+  });
+  saveToLocalStorage(todoList);
+  renderList(todoList);
+};
+
+const clearButton = document.querySelector('.clear-btn');
+clearButton.addEventListener('click', () => {
+  clearCompletedTasks();
+});
+
+window.onload = () => {
+  todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+  renderList(todoList);
+};
+
+const addForm = document.querySelector('.add-form');
+addForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const addInput = document.querySelector('.add-input');
+  const description = addInput.value.trim();
+  if (description !== '') {
+    addTask(description);
+    addInput.value = '';
+  }
+});
+
+ToDoListContainer.addEventListener('click', (e) => {
+  const taskElement = e.target.closest('.to-do-tasks');
+  if (taskElement) {
+    const taskId = Number(taskElement.dataset.taskId);
+    const checkBox = taskElement.querySelector('.check-box');
+    const taskDescription = taskElement.querySelector('.task-description');
+    const moreLogo = taskElement.querySelector('.more-logo');
+
+    if (e.target === checkBox) {
+      const completed = !checkBox.classList.contains('completed');
+      checkBox.classList.toggle('completed');
+      taskDescription.classList.toggle('line-through');
+      checkBox.textContent = completed ? '✓' : '';
+      todoList.forEach((task) => {
+        if (task.id === taskId) {
+          task.completed = completed;
+        }
+      });
+      saveToLocalStorage(todoList);
+    } else if (e.target === moreLogo) {
+      // Handle more logo click event if needed
+    }
+  }
+});
+
+ToDoListContainer.addEventListener('input', (e) => {
+  const taskElement = e.target.closest('.to-do-tasks');
+  if (taskElement) {
+    const taskId = Number(taskElement.dataset.taskId);
+    const taskDescription = taskElement.querySelector('.task-description');
+    if (e.target === taskDescription) {
+      const newDescription = taskDescription.textContent.trim();
+      editTaskDescription(taskId, newDescription);
+    }
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,16 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
     ],
   },
 };
