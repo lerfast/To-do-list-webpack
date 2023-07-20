67a9194dccec7118241021bced461e975835f655
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-07-20 17:18:39 -0500

    Updating for linters
    Running Linters

--- src/utils.test.js
+++ src/utils.test.js
@@ -7,19 +7,24 @@ import {
   moveTaskToTop,
   moveTaskToBottom,
 } from './utils.js';
+
 class LocalStorageMock {
   constructor() {
     this.store = {};
   }
+
   getItem(key) {
     return this.store[key] || null;
   }
+
   setItem(key, value) {
     this.store[key] = value.toString();
   }
+
   removeItem(key) {
     delete this.store[key];
   }
+
   clear() {
     this.store = {};
   }
